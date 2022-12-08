67f5105ab905d3db1de86238fa2d6a6a3df84dbc
Alex Ban <alexbanut10@gmail.com>
2022-12-08 20:44:22 +0200

    obj exercises lvl 3

--- objLvl2.js
+++ objLvl2.js
@@ -132,3 +132,91 @@ const personAccount = {
 		return `Total income: ${this.incomes.crypto + this.incomes.salary + this.incomes.stocks + this.incomes.rents}$`;
 	}
 }
+
+// 2); 3); 4)
+const userz = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+];
+
+// 2) a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+
+const signUp = (userName) => {
+	n = 0
+	while(n < userz.length) {
+		n++;
+		if(userz[n] !== userName) {
+			return userz.push({ username: userName })
+		} else {
+			return 'You are already in the database!'
+		}
+	}
+}
+
+    const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+];
